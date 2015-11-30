969e82f5799695e759f55547771b7d80843e544c
wu736139669 <wu736139669@736139669@qq.com>
2015-11-30 08:44:32 +0800

    new

--- Doctor/View/MainTabBarViewController.m
+++ Doctor/View/MainTabBarViewController.m
@@ -27,9 +27,9 @@
         _serviceNav = [[UINavigationController alloc] initWithRootViewController:[[ServiceViewController alloc] init]];
         _navigateNav = [[UINavigationController alloc] initWithRootViewController:[[NavigateViewController alloc] init]];
         _myNav = [[UINavigationController alloc] initWithRootViewController:[[MyViewController alloc] init]];
-        [_navigateNav setTabBarItem:[[UITabBarItem alloc] initWithTitle:@"职能导航" image:[UIImage imageNamed:@"seriviceno"] selectedImage:[UIImage imageNamed:@"seriviced"]]];
+        [_navigateNav setTabBarItem:[[UITabBarItem alloc] initWithTitle:@"职能导航" image:[UIImage imageNamed:@"navno"] selectedImage:[UIImage imageNamed:@"naved"]]];
         
-        [_serviceNav setTabBarItem:[[UITabBarItem alloc] initWithTitle:@"医疗服务" image:[UIImage imageNamed:@"seriviceno"] selectedImage:[UIImage imageNamed:@"seriviced"]]];
+        [_serviceNav setTabBarItem:[[UITabBarItem alloc] initWithTitle:@"医疗服务" image:[UIImage imageNamed:@"serviceno"] selectedImage:[UIImage imageNamed:@"serviced"]]];
         
         [_myNav setTabBarItem:[[UITabBarItem alloc] initWithTitle:@"个人中心" image:[UIImage imageNamed:@"myno"] selectedImage:[UIImage imageNamed:@"myed"]]];
         self.viewControllers = @[_navigateNav,_serviceNav, _myNav];
--- Doctor/View/MyViewController.m
+++ Doctor/View/MyViewController.m
@@ -8,7 +8,7 @@
 
 #import "MyViewController.h"
 #import "MyTopView.h"
-@interface MyViewController ()
+@interface MyViewController ()<UITableViewDelegate,UITableViewDataSource>
 {
     MyTopView *_myTopView;
 }
@@ -21,12 +21,68 @@
     // Do any additional setup after loading the view from its nib.
     self.title = @"个人中心";
     
-    _myTopView = [Ash_UIUtil instanceXibView:@"MyTopView"];
-    
-    _myTopView.frame = CGRectMake(0, 0, kScreenWidth, 200);
+
     
 }
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    if (!_myTopView)
+    {
+        _myTopView = [Ash_UIUtil instanceXibView:@"MyTopView"];
+        _myTopView.frame = CGRectMake(0, 0, kScreenWidth, 200);
+        _tableView.tableHeaderView = _myTopView;
+    }
 
+    
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    int row = 0;
+    switch (section) {
+        case 0:
+            row = 3;
+            break;
+        case 1:
+            row = 1;
+            break;
+        case 2:
+            row = 2;
+            break;
+        case 3:
+            row = 2;
+            break;
+            
+        default:
+            break;
+    }
+    return row;
+}
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 4;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return 0.1;
+    }
+    return 5.0;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 5.0;
+}
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString* cellIndentifier = @"cell";
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:cellIndentifier];
+    if(!cell){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIndentifier];
+    }
+    cell.textLabel.text = @"测试";
+    return cell;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
